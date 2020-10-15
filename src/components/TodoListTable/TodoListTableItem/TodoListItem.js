a2a4904479e2d1e3dba1300e094b744d54126447
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-15 19:07:14 +0200

    Add component

---
+++ src/components/TodoListTable/TodoListTableItem/TodoListItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteItem, updateItemDone } from '../../../actions/todoListActions';
+import { ItemWrapper, P, DoneAndTrashWrapper } from './TodoListItem.style';
+
+const TodoListTableItem = ({ id, name, priority, done }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <ItemWrapper>
+        <P grow={10}>{name}</P>
+        <P grow={1}>{priority}</P>
+        <DoneAndTrashWrapper grow={1}>
+          <label style={{ position: 'absolute' }} htmlFor='checkbox'></label>
+          <input
+            type='checkbox'
+            id='checkbox'
+            name='checkbox'
+            value={done}
+            checked={done}
+            onChange={(e) => dispatch(updateItemDone(id, !done))}
+          />
+          <button onClick={() => dispatch(deleteItem(id))}>del</button>
+        </DoneAndTrashWrapper>
+      </ItemWrapper>
+    </>
+  );
+};
+
+export default TodoListTableItem;
