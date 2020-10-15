2e6959e93a74b83cca40f883b8b5d2bdae41254d
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-15 19:05:14 +0200

    Add Normalize

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Normalize } from 'styled-normalize';
 import store from './store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
+    <Normalize />
     <App />
   </Provider>,
   document.getElementById('root')
