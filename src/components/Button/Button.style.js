c2c134d7d26c0e585aadaf080926347794c5ed6b
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-16 11:44:16 +0200

    Add styling

---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  background-color: #494430;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 200px;
+  padding: 0.5rem 0.5rem;
+  border: none;
+`;
