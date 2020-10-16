34a53a90e779b888a00753471996248b62c6481f
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-16 12:59:59 +0200

    Add sortReducer

--- src/store.js
+++ src/store.js
@@ -3,10 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { layoutAddTaskFormReducer } from './reducers/layoutReducers';
 import { todoListReducer } from './reducers/todoListReducers';
+import { sortReducer } from './reducers/sortReducers';
 
 const reducer = combineReducers({
   layoutAddTaskForm: layoutAddTaskFormReducer,
   todoList: todoListReducer,
+  sort: sortReducer,
 });
 
 const todoListFromStorage = localStorage.getItem('todoList')
