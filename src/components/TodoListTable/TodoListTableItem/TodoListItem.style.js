2f974a24508da5d3e492d4376d2e3facfa833296
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-16 11:46:14 +0200

    Add TrashIcon visibility on parent hover

--- src/components/TodoListTable/TodoListTableItem/TodoListItem.style.js
+++ src/components/TodoListTable/TodoListTableItem/TodoListItem.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ReactComponent as Trash } from '../../../assets/trashIcon.svg';
 
 export const ItemWrapper = styled.div`
   display: flex;
@@ -8,12 +9,16 @@ export const ItemWrapper = styled.div`
   padding: 0 1rem;
   border: 1px solid #e1e1e1;
   border-top: none;
+  &:hover > div > button {
+    visibility: visible;
+  }
 `;
 
 export const P = styled.p`
   flex-grow: ${(props) => props.grow};
   width: 80px;
   color: #707070;
+  overflow: hidden;
 `;
 
 export const DoneAndTrashWrapper = styled.div`
@@ -22,4 +27,16 @@ export const DoneAndTrashWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  & button {
+    visibility: hidden;
+    background-color: white;
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+export const TrashIcon = styled(Trash)`
+  width: 1rem;
+  height: 1rem;
+  color: #707070;
 `;
