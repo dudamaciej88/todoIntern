23282b39bd2bb3cd6e8342882057beb43921fb2f
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-18 11:53:22 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TodoListScreen from './screens/TodoListScreen';
-
+import AddTaskForm from './components/AddTaskForm/AddTaskForm';
 
 function App() {
   return (
-    <><TodoListScreen/></>
+    <Router>
+      <Route path='/addtask' component={AddTaskForm} />
+      <Route path='/' component={TodoListScreen} />
+    </Router>
   );
 }
 
