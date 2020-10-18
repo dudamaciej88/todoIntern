c1327daddc9012c2489bae1c82f6fa6cdf2a2ce6
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-18 15:46:55 +0200

    .

--- src/actions/layoutActions.js
+++
@@ -1,5 +0,0 @@
-import {LAYOUT_ADDTASKFORM_TOGGLE} from '../constants/layoutConstants';
-
-export const toggleAddTaskForm = (value) => (dispatch) => {
-    dispatch({type: LAYOUT_ADDTASKFORM_TOGGLE, payload: value})
-}
