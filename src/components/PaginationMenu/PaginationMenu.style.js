e2aa161850c18a9d2127872dfd07953d8eafbf83
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-18 11:55:39 +0200

    Add styling

---
+++ src/components/PaginationMenu/PaginationMenu.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 1rem 1rem;
+  border: 1px solid #e1e1e1;
+  border-top: none;
+`;
