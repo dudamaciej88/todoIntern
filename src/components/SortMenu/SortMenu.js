f792c5ae2e467322b8fa90ed0ec0ee5840ed0dea
dudamaciej88 <dudamaciej88@gmail.com>
2020-10-18 15:48:31 +0200

    Add ArrowButton

--- src/components/SortMenu/SortMenu.js
+++ src/components/SortMenu/SortMenu.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Wrapper } from './SortMenu.style';
 import { setSort } from '../../actions/sortActions';
 import { useDispatch } from 'react-redux';
+import ArrowButton from '../ArrowButton/ArrowButton'
 
-const SortMenu = ({sortByValue}) => {
+const SortMenu = ({sortByValue, disabled}) => {
   const dispatch = useDispatch();
 
   return (
     <Wrapper>
-      <button onClick={() => dispatch(setSort(sortByValue, 'up'))}>up</button>
-      <button onClick={() => dispatch(setSort(sortByValue, 'down'))}>down</button>
+      <ArrowButton direction={'up'} onClick={() => dispatch(setSort(sortByValue, 'up'))}></ArrowButton>
+      <ArrowButton direction={'down'} onClick={() => dispatch(setSort(sortByValue, 'down'))}></ArrowButton>
     </Wrapper>
   );
 };
